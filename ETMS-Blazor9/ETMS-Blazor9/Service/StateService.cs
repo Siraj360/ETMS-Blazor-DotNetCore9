2c32c876c50f38f35df5ce2eb75a5425e231055f
Siraj Gadhia <Siraj360@users.noreply.github.com>
2025-01-10 15:44:23 -0500

    Update
    Update

---
+++ ETMS-Blazor9/ETMS-Blazor9/Service/StateService.cs
@@ -0,0 +1,28 @@
+﻿namespace Stock360_2025.Service;
+using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using Microsoft.Extensions.DependencyInjection;
+public class StateService 
+{
+
+    private readonly BehaviorSubject<string> _title;
+
+    public StateService()
+    {
+        _title = new BehaviorSubject<string>("Welcome to STOCK 360 Blazor App");
+    }
+
+    public IObservable<string> TitleObservable => _title.AsObservable();
+
+    public void UpdateTitle(string newValue)
+    {
+        _title.OnNext(newValue);
+    }
+
+    public string GetTitle()
+    {
+        return _title.Value;
+    }
+}
+
